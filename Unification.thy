abba4085f00984148e35159a5cc56e7bb1f7f725
emfuthal <ewaldboss@hotmail.com>
2017-11-13 17:13:38 +0100

    definitions of assignment 1

--- Unification.thy
+++ Unification.thy
@@ -4,43 +4,139 @@ theory Unification imports Main begin
 
 (* Assignment 1 *)
 
+(****************************** definitions ******************************)
+
 datatype ('f, 'v) "term" = Var 'v | Fun 'f "('f, 'v) term list"
 
 fun fv :: "('f, 'v) term \<Rightarrow> 'v set" where
+  "fv (Var x) = {x}"
+| "fv (Fun f xs) = (\<Union>x \<in> (set xs). fv x)"
 
 type_synonym ('f, 'v) subst = "'v \<Rightarrow> ('f, 'v) term"
 
 fun sapply :: "('f, 'v) subst \<Rightarrow> ('f, 'v) term \<Rightarrow> ('f, 'v) term" (infixr "\<cdot>" 67) where 
-  "sapply _ x = x" 
+  "sapply \<sigma> (Var x) = \<sigma> x"
+| "sapply \<sigma> (Fun g xs) = Fun g (map (sapply \<sigma>) xs)"
 
 fun scomp :: "('f, 'v) subst \<Rightarrow> ('f, 'v) subst \<Rightarrow> ('f, 'v) subst" (infixl "\<circ>s" 75) where
+  "scomp \<sigma> \<tau> = (\<lambda>x. \<sigma> \<cdot> (\<tau> x))"
+
+
+
+(******************************** lemmata **********************************) 
 
 lemma fv_sapply: "fv (\<sigma> \<cdot> t) = (\<Union>x \<in> fv t. fv (\<sigma> x))"
-  sorry
+  apply (induction t rule: fv.induct)
+   apply (auto)
+  done
 
-lemma sapply_cong:
-  assumes "\<And>x. x \<in> fv t \<Longrightarrow> \<sigma> x = \<tau> x"
-  shows "\<sigma> \<cdot> t = \<tau> \<cdot> t"
-  sorry
+lemma sapply_cong: "\<lbrakk>\<And>x. x \<in> fv t \<Longrightarrow> \<sigma> x = \<tau> x \<rbrakk> \<Longrightarrow> \<sigma> \<cdot> t = \<tau> \<cdot> t"
+  apply (induction t rule: fv.induct)
+   apply (auto)
+  done
 
-lemma scomp_sapply: "(\<sigma> \<circ>s \<tau>) x = \<sigma> \<cdot> (\<tau> \<cdot> x)"
-  sorry
+lemma scomp_sapply [simp]: "(\<sigma> \<circ>s \<tau>) x = \<sigma> \<cdot> (\<tau> x)"
+  by simp
 
-lemma sapply_scomp_distr: "(\<sigma> \<circ>s \<tau>) \<cdot> t = \<sigma> \<cdot> (\<tau> \<cdot> t)"
-  sorry
+lemma sapply_scomp_distr [simp]: "(\<sigma> \<circ>s \<tau>) \<cdot> t = \<sigma> \<cdot> (\<tau> \<cdot> t)"
+  apply simp
+  apply (induction t rule: fv.induct)
+   apply auto
+  done
 
 lemma scomp_assoc: "(\<sigma> \<circ>s \<tau>) \<circ>s \<rho> = \<sigma> \<circ>s (\<tau> \<circ>s \<rho>)"
-  sorry
+proof -
+  have "(\<sigma> \<circ>s \<tau>) \<circ>s \<rho> = (\<lambda>x. (\<sigma> \<circ>s \<tau>) \<cdot> \<rho> x)" by simp
+  also have "... = (\<lambda>x. \<sigma> \<cdot> \<tau> \<cdot> \<rho> x)" by (simp only: sapply_scomp_distr)
+  also have "... = \<sigma> \<circ>s (\<tau> \<circ>s \<rho>)" by simp
+  finally show ?thesis .
+qed
 
 lemma scomp_var [simp]: "\<sigma> \<circ>s Var = \<sigma>"
-  sorry
+  by simp
+
+lemma var_sapply: "Var \<cdot> t = t"
+  apply (induction t rule: fv.induct)
+  by (auto simp add: map_idI)
 
 lemma var_scomp [simp]: "Var \<circ>s \<sigma> = \<sigma>"
+  by (simp add: var_sapply)
+
+
+(********************************** definitions ****************************)
+
+fun sdom :: "('f, 'v) subst \<Rightarrow> 'v set" where
+  "sdom \<sigma> = {x | x. \<sigma> x \<noteq> Var x}"
+
+fun ran :: "('f, 'v) subst \<Rightarrow> ('f, 'v) term set" where
+  "ran \<sigma> = {\<sigma> x | x. x \<in> sdom \<sigma>}"
+
+fun svran :: "('f, 'v) subst \<Rightarrow> 'v set" where
+  "svran \<sigma> = (\<Union> t \<in> ran \<sigma>. fv t)"
+
+
+(************************************ lemmata ******************************)
+
+lemma sdom_var [simp]: "sdom Var = {}"
+  by simp
+
+lemma svran_var [simp]: "svran Var = {}"
+  by simp
+
+lemma sdom_single_non_trivial [simp]: "t \<noteq> Var x \<Longrightarrow> sdom (Var (x := t)) = {x}"
+  by simp
+
+lemma svran_single_non_trivial [simp]: "t \<noteq> Var x \<Longrightarrow> svran (Var (x := t)) = fv t"
+  by simp
+
+(*lemma "\<sigma> \<cdot> t = (fv t - sdom \<sigma>) \<union> ran \<sigma>"*)
+
+
+lemma fv_sapply_sdom_svran: "x \<in> fv (\<sigma> \<cdot> t) \<Longrightarrow> x \<in> (fv t - sdom \<sigma>) \<union> svran \<sigma>"
+  apply (induction t rule: fv.induct)
   sorry
 
 
+lemma sdom_scomp: "sdom (\<sigma> \<circ>s \<tau>) \<subseteq> sdom \<sigma> \<union> sdom \<tau>"
+  sorry
+
+lemma svran_scomp: "svran (\<sigma> \<circ>s \<tau>) \<subseteq> svran \<sigma> \<union> svran \<tau>"
+  sorry
 
 
+(**************************************** definitions *********************************)
+
+type_synonym ('f, 'v) equation = "('f, 'v) term \<times> ('f, 'v) term"
+
+type_synonym ('f, 'v) equations = "('f, 'v) equation list"
+
+fun fv_eq :: "('f, 'v) equation \<Rightarrow> 'v set \<times> 'v set" where
+  "fv_eq (a, b) = (fv a, fv b)"
+
+fun fv_eqs :: "('f, 'v) equations \<Rightarrow> ('v set \<times> 'v set) list" where
+  "fv_eqs l = map fv_eq l"
+
+fun sapply_eq :: "('f, 'v) subst \<Rightarrow> ('f, 'v) equation \<Rightarrow> ('f, 'v) equation" where
+  "sapply_eq \<sigma> (a,b) = (sapply \<sigma> a, sapply \<sigma> b)"
+
+fun sapply_eqs :: "('f, 'v) subst \<Rightarrow> ('f, 'v) equations \<Rightarrow> ('f, 'v) equations" where
+  "sapply_eqs \<sigma> l = map (sapply_eq \<sigma>) l"
+
+
+(******************************** lemmata *************************************)
+
+lemma fv_sapply_eq: "fv_eq (sapply_eq \<sigma> (a,b)) = ((\<Union>x \<in> fv a. fv (\<sigma> \<cdot> a)), (\<Union>y \<in> fv b. fv (\<sigma> \<cdot> b)))"
+  sorry
+
+lemma fv_sapply_eqs: "set (fv_eqs (sapply_eqs \<sigma> l)) = (\<Union>x \<in> set l. fv_eq (sapply_eq \<sigma> x))"
+
+lemma sapply_scomp_distrib_eq
+
+lemma sapply_scomp_distrib_eqs
+
+
+  
+  
 
 
 
