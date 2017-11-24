b9019b775aebe202ceef86af715d734f250b36ef
Symphorien Gibol <symphorien+git@xlumurb.eu>
2017-11-24 09:50:24 +0100

    Deduction.thy: switch back constraint systems to list

--- Deduction.thy
+++ Deduction.thy
@@ -47,7 +47,7 @@ Constraint "msg list " "msg list " "msg " ( "((2_/|_)/} _) " [67,67,67]66)
 they are popped from the head of the first list and are push to the top
 of the second one *)
 
-type_synonym system = "constraint set"
+type_synonym system = "constraint list"
 
 (* (a) *)
 (* functions lifted to msg list get a postfix l
@@ -58,7 +58,7 @@ definition sapplyl::"subst_msg \<Rightarrow> msg list \<Rightarrow> msg list" wh
 definition sapplyc:: "subst_msg \<Rightarrow> constraint \<Rightarrow> constraint" where
 "sapplyc s c = (case c of  a|m}t \<Rightarrow> (sapplyl s a)|(sapplyl s m)}( sapply_msg s t))"
 definition sapplys:: "subst_msg \<Rightarrow> system \<Rightarrow> system" where
-"sapplys s cs = image (sapplyc s) cs"
+"sapplys s cs = map (sapplyc s) cs"
 
 
 lemma sapplyc_scomp:"sapplyc (scomp_msg t s) c = sapplyc t (sapplyc s (c))"
@@ -72,7 +72,7 @@ definition fvl:: "msg list \<Rightarrow> var set" where
 definition fvc:: "constraint \<Rightarrow> var set" where
 "fvc c = (case c of a|m}t \<Rightarrow> (fvl a) \<union> (fvl m) \<union> (fv_msg t))"
 definition fvs:: "system \<Rightarrow> var set" where
-"fvs cs = (\<Union>c \<in> cs. fvc c)"
+"fvs cs = (\<Union>c \<in> set cs. fvc c)"
 
 (* (b) *)
 (* solution set *)
@@ -80,28 +80,28 @@ definition solved:: "constraint \<Rightarrow> bool" where
 "solved c = (case c of (m|a}t) \<Rightarrow> ((set m \<union> set a) \<turnstile> t))"
 
 definition sol:: "system \<Rightarrow> subst_msg set" where
-"sol cs = {s. (\<forall> c \<in> cs. solved (sapplyc s c))}"
+"sol cs = {s. (\<forall> c \<in> set cs. solved (sapplyc s c))}"
 
-lemma solE: "s \<in> sol cs \<Longrightarrow> ((\<And>c. c \<in> cs \<Longrightarrow> solved (sapplyc s c)) \<Longrightarrow> P) \<Longrightarrow>P"
+lemma solE: "s \<in> sol cs \<Longrightarrow> ((\<And>c. c \<in> set cs \<Longrightarrow> solved (sapplyc s c)) \<Longrightarrow> P) \<Longrightarrow>P"
   by(auto simp add:sol_def)
 
-lemma solI: "(\<And>c. c \<in> cs \<Longrightarrow> solved (sapplyc s c)) \<Longrightarrow> s \<in> sol cs"
+lemma solI: "(\<And>c. c \<in> set cs \<Longrightarrow> solved (sapplyc s c)) \<Longrightarrow> s \<in> sol cs"
   by(auto simp add:sol_def)
 
 (* lemmas *)
 
-lemma sol_concat: "sol(cs \<union> cs') = sol(cs) \<inter> sol(cs')"
+lemma sol_concat: "sol(cs @ cs') = sol(cs) \<inter> sol(cs')"
 proof((rule equalityI;rule subsetI),rule IntI)
 qed(auto simp add:sol_def)
 
 lemma "sol_scomp": "t \<in> sol(sapplys s cs) \<Longrightarrow> (scomp_msg t s) \<in> sol(cs)"
 proof(rule solI, erule solE)
   fix c
-  assume w:"c \<in> cs"
-  assume x:"\<And>d. d \<in> (sapplys s cs) \<Longrightarrow> solved (sapplyc t d)"
+  assume w:"c \<in> set cs"
+  assume x:"\<And>d. d \<in> set (sapplys s cs) \<Longrightarrow> solved (sapplyc t d)"
   have "solved (sapplyc (scomp_msg t s) c) = solved (sapplyc t (sapplyc s c))" (is "_ = solved (sapplyc t (?d))")
     by(simp add: sapplyc_scomp)
-  moreover have "?d \<in> (sapplys s cs)" by(simp add:w sapplys_def)
+  moreover have "?d \<in> set (sapplys s cs)" by(simp add:w sapplys_def)
   ultimately show "solved (sapplyc (scomp_msg t s) c)"
     by(simp add:x)
 qed
@@ -109,24 +109,24 @@ qed
 (* (c) *)
 inductive rer1 :: "constraint \<Rightarrow> subst_msg \<Rightarrow> system \<Rightarrow> bool "
 ("(_)/\<leadsto>1[_]/ (_)" [64,64,64]63)    where
-rer1_unify: "(\<And> z. t\<noteq>Variable z) \<Longrightarrow> u \<in> set m \<union> set a \<Longrightarrow> unify_msg [(t,u)] = Some s \<Longrightarrow> (a|m}t)\<leadsto>1[s] {}"
-| rer1_comp_hash:"(a|m} (Hash t)) \<leadsto>1[Variable] {(a|m} t)}"
-| rer1_comp_concat:"(a|m} (Concat t u)) \<leadsto>1[Variable] {a|m} t, a|m} u}"
-| rer1_comp_sym_encrypt:"(a|m} (Sym_encrypt t u)) \<leadsto>1[Variable] {a|m} t, a|m} u}"
-| rer1_comp_pub_encrypt:"(a|m} (Pub_encrypt t u)) \<leadsto>1[Variable] {a|m} t, a|m} u}"
-| rer1_comp_sign:"(a|m} (Sign t (Const ''intruder''))) \<leadsto>1[Variable] {a|m} t}"
-| rer1_proj: "x=Concat u v \<Longrightarrow> (head@x#tail) | m } t \<leadsto>1[Variable] {((u#v#head@tail) | (x#m) } t)}"
+rer1_unify: "(\<And> z. t\<noteq>Variable z) \<Longrightarrow> u \<in> set m \<union> set a \<Longrightarrow> unify_msg [(t,u)] = Some s \<Longrightarrow> (a|m}t)\<leadsto>1[s] []"
+| rer1_comp_hash:"(a|m} (Hash t)) \<leadsto>1[Variable] [(a|m} t)]"
+| rer1_comp_concat:"(a|m} (Concat t u)) \<leadsto>1[Variable] [ a|m} t, a|m}u ]"
+| rer1_comp_sym_encrypt:"(a|m} (Sym_encrypt t u)) \<leadsto>1[Variable] [ a|m}t, a|m}u ]"
+| rer1_comp_pub_encrypt:"(a|m} (Pub_encrypt t u)) \<leadsto>1[Variable] [ a|m}t, a|m}u ]"
+| rer1_comp_sign:"(a|m} (Sign t (Const ''intruder''))) \<leadsto>1[Variable] [a|m} t]"
+| rer1_proj: "x=Concat u v \<Longrightarrow> (head@x#tail) | m } t \<leadsto>1[Variable] [ (u#v#head@tail) | (x#m) } t ]"
 | rer1_sdec: "x=Sym_encrypt u k \<Longrightarrow> (head@x#tail) | m } t \<leadsto>1[Variable]
- {  ((u#head@tail) | (x#m) } t) ,
-    ((head@tail) | (x#m) } k)     }"
+ [  ((u#head@tail) | (x#m) } t) ,
+    ((head@tail) | (x#m) } k)     ]"
 | rer1_adec: "x=Pub_encrypt u (Variable ''intruder'') \<Longrightarrow> (head@x#tail) | m } t \<leadsto>1[Variable]
- { ((u#head@tail) | (x#m) } t) }"
+ [ ((u#head@tail) | (x#m) } t) ]"
 | rer1_ksub: "Pub_encrypt u (Variable agent) \<in> set a \<Longrightarrow>
 s=(%v. Variable (if v=agent then ''intruder'' else v)) \<Longrightarrow>
-a | m } t  \<leadsto>1[s] { sapplyc s (a | m } t) }"
+a | m } t  \<leadsto>1[s] [ sapplyc s (a|m}t) ]"
 
 inductive rer :: "system \<Rightarrow> subst_msg \<Rightarrow> system \<Rightarrow> bool " ("(_)/\<leadsto>[_]/ _ " [73,73,73]72) where
-"c \<leadsto>1[s] cs \<Longrightarrow> (insert c cs') \<leadsto>[s] (cs \<union> (sapplys s cs'))"
+"c \<leadsto>1[s] cs \<Longrightarrow> (head@c#tail) \<leadsto>[s] (cs @ (sapplys s (head@tail)))"
 
 inductive rer_star :: "system \<Rightarrow> subst_msg \<Rightarrow> system \<Rightarrow> bool" ("(_)/\<leadsto>*[_]/ _ " [73,73,73]72) where
 rer_star_id: "cs \<leadsto>*[Variable] cs"
@@ -137,7 +137,7 @@ inductive simplec:: "constraint \<Rightarrow> bool" where
 "fvl a = {} \<Longrightarrow> fvl m = {} \<Longrightarrow> simplec (a|m}t)"
 
 definition simples:: "system \<Rightarrow> bool" where
-"simples cs = (\<forall> c \<in> cs. simplec c)"
+"simples cs = (\<forall> c \<in> set cs. simplec c)"
 
 inductive is_red:: "system \<Rightarrow> subst_msg \<Rightarrow> bool" where
 "cs \<leadsto>*[s] cs' \<Longrightarrow> simples cs' \<Longrightarrow> t \<in> sol(cs') \<Longrightarrow> is_red cs (scomp_msg t s)"
